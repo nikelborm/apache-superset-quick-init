98118d637afffc4365da8bd09d78390ff70be4a5
nikelborm <evadev@duck.com>
2024-12-16 04:06:49 +0300

    quick save: Mon Dec 16 04:06:49 MSK 2024

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # apache-superset-quick-init
 
+## How to use
+
+```bash
+./main.sh
+cd ./tmp
+nano ./docker/.env
+nano ./docker/superset-websocket/config.json
+# choose tag you want: 'latest' or something specific like '4.1.1'
+export TAG='latest'
+docker compose up -d --pull always --no-build
+# follow logs until completely deployed
+docker compose logs -f
+```
+
 ## TODO
 
 1. create separate script for quick registering of a new database into current superset instance
--- main.sh
+++ main.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
+mkdir tmp
+cd tmp
 temp_dir=$(mktemp -d);
-original_dir=$(cwd);
 temp_env=$temp_dir/.temp.env;
 temp_js=$temp_dir/index.mjs;
+
 curl -Lo $temp_js https://github.com/nikelborm/download-github-folder/releases/latest/download/index.js;
 curl -Lo $temp_env https://github.com/nikelborm/download-github-folder/releases/latest/download/template.env;
+
+sed -i "s/PATH_TO_LOCAL_DIR_INTO_WHICH_CONTENTS_OF_REPO_DIR_WILL_BE_PUT='\/tmp\/tmp.pPuwKB2gSZ\/docker'/PATH_TO_LOCAL_DIR_INTO_WHICH_CONTENTS_OF_REPO_DIR_WILL_BE_PUT='$(pwd | sed 's_/_\\/_g')\/docker'/" $temp_env
+
+# to set token
 nano $temp_env;
 node --env-file=$temp_env $temp_js;
-curl -Lo ./docker-compose-non-dev.yml https://raw.githubusercontent.com/apache/superset/master/docker-compose-non-dev.yml;
-echo -e "\nnetworks:\n  default:\n    name: apache_superset_network" >> docker-compose-non-dev.yml;
+
+curl -Lo ./compose.yml https://raw.githubusercontent.com/apache/superset/master/docker-compose-image-tag.yml;
+echo -e "\nnetworks:\n  default:\n    name: apache_superset_network" >> compose.yml;
 rm -rf $temp_dir
+
+# fixes bug of download-github-folder util
+find ./docker -type f -name '*.sh' -exec chmod +x '{}' \;
+
+sed -i "s/DEV_MODE=true/DEV_MODE=false/" ./docker/.env
+
+# to secure envs
+sudo chmod -R og= ./docker/superset-websocket
+sudo chmod -R og= ./docker/.env
+
+
+echo 'psycopg2-binary' > ./docker/requirements-local.txt
