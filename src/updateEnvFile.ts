a065cf73d8ca7e7e7fcb33dabba609096868c5b4
nikelborm <evadev@duck.com>
2025-03-21 08:34:12 +0000

    refactor updateEnvFile.ts

--- src/updateEnvFile.ts
+++ src/updateEnvFile.ts
@@ -3,30 +3,27 @@ import { Path } from '@effect/platform/Path';
 import { all, fn } from 'effect/Effect';
 import { generateRandomPassword } from './generateRandomPassword.js';
 
-export const updateEnvFile = fn(function* (basePath: string) {
-  const fs = yield* FileSystem;
-  const path = yield* Path;
+export const updateEnvFile = fn('updateEnvFile')(function* (basePath: string) {
+  const [fs, path] = yield* all([FileSystem, Path]);
+
   const envFilePath = path.join(basePath, 'docker', '.env');
 
-  const { dbPass, envFile, examplesPass, supersetSecretKey } = yield* all(
-    {
-      envFile: fs.readFileString(envFilePath, 'utf8'),
-      dbPass: generateRandomPassword,
-      examplesPass: generateRandomPassword,
-      supersetSecretKey: generateRandomPassword,
-    },
-    { concurrency: 'unbounded' },
-  );
+  const { dbPass, envFile, examplesPass, supersetSecretKey } = yield* all({
+    envFile: fs.readFileString(envFilePath, 'utf8'),
+    dbPass: generateRandomPassword,
+    examplesPass: generateRandomPassword,
+    supersetSecretKey: generateRandomPassword,
+  });
 
   const newEnvFile = envFile
-    .replaceAll(/^(DEV_MODE)=.*/gm, '$1=false')
-    .replaceAll(/^(FLASK_DEBUG)=.*/gm, '$1=false')
+    .replaceAll(
+      /^(DEV_MODE|FLASK_DEBUG|PUPPETEER_SKIP_CHROMIUM_DOWNLOAD)=.*/gm,
+      '$1=false',
+    )
     .replaceAll(/^(SUPERSET_ENV)=.*/gm, '$1=production')
     .replaceAll(/^(SUPERSET_LOAD_EXAMPLES)=.*/gm, '$1=no')
     .replaceAll(/^(ENABLE_PLAYWRIGHT)=.*/gm, '$1=true')
-    .replaceAll(/^(PUPPETEER_SKIP_CHROMIUM_DOWNLOAD)=.*/gm, '$1=false')
-    .replaceAll(/^(DATABASE_PASSWORD)=.*/gm, `$1="${dbPass}"`)
-    .replaceAll(/^(POSTGRES_PASSWORD)=.*/gm, `$1="${dbPass}"`)
+    .replaceAll(/^(DATABASE_PASSWORD|POSTGRES_PASSWORD)=.*/gm, `$1="${dbPass}"`)
     .replaceAll(/^(EXAMPLES_PASSWORD)=.*/gm, `$1="${examplesPass}"`)
     .replaceAll(/^(SUPERSET_SECRET_KEY)=.*/gm, `$1="${supersetSecretKey}"`)
     .replaceAll(
