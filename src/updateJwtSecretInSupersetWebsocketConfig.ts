a83706dad27a28904821369de1d3b372847afe60
nikelborm <evadev@duck.com>
2025-03-23 08:34:12 +0000

    refactor updateJwtSecretInSupersetWebsocketConfig.ts

--- src/updateJwtSecretInSupersetWebsocketConfig.ts
+++ src/updateJwtSecretInSupersetWebsocketConfig.ts
@@ -11,11 +11,10 @@ import {
 } from 'effect/Schema';
 import { generateRandomPassword } from './generateRandomPassword.js';
 
-export const updateJwtSecretInSupersetWebsocketConfig = fn(function* (
-  basePath: string,
-) {
-  const fs = yield* FileSystem;
-  const path = yield* Path;
+export const updateJwtSecretInSupersetWebsocketConfig = fn(
+  'updateJwtSecretInSupersetWebsocketConfig',
+)(function* (basePath: string) {
+  const [fs, path] = yield* all([FileSystem, Path]);
 
   const supersetWebsocketConfigPath = path.join(
     basePath,
@@ -24,15 +23,12 @@ export const updateJwtSecretInSupersetWebsocketConfig = fn(function* (
     'config.json',
   );
 
-  const { configFileParsed, jwtSecret } = yield* all(
-    {
-      configFileParsed: fs
-        .readFileString(supersetWebsocketConfigPath, 'utf8')
-        .pipe(flatMap(decodeSupersetWebsocketConfig)),
-      jwtSecret: generateRandomPassword,
-    },
-    { concurrency: 'unbounded' },
-  );
+  const { configFileParsed, jwtSecret } = yield* all({
+    configFileParsed: fs
+      .readFileString(supersetWebsocketConfigPath, 'utf8')
+      .pipe(flatMap(decodeSupersetWebsocketConfig)),
+    jwtSecret: generateRandomPassword,
+  });
 
   const config = yield* encodeSupersetWebsocketConfig({
     ...configFileParsed,
